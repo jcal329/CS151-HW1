90c1ca8b354ea4f5fd6da8ec39421d79efd642c1
Jon Lindberg <jcal329@gmail.com>
2020-09-10 16:51:47 -0700

    added speech bubble

--- HW1/src/AsciiGreeter.java
+++ HW1/src/AsciiGreeter.java
@@ -1,9 +1,11 @@
 public class AsciiGreeter {
     public static void main(String[] args) {
         System.out.println(
-                "\t /\\____/\\\n" +
-                        "\t/  _    _\\\n" +
-                        "\t      o"
+                "\t /\\____/\\    ____________\n" +
+                        "\t/  _    _\\  /            |\n" +
+                        "\t      o    <   Less talk |\n" +
+                        "\t\t        \\ more coffee|\n" +
+                        "\t\t         \\___________|"
         );
     }
 }
