d3071e7a8f9c52c32f1de53c96ac6c18abe42658
Jon Lindberg <jcal329@gmail.com>
2020-09-10 16:41:09 -0700

    created cat face

---
+++ HW1/src/AsciiGreeter.java
@@ -0,0 +1,9 @@
+public class AsciiGreeter {
+    public static void main(String[] args) {
+        System.out.println(
+                "\t /\\____/\\\n" +
+                        "\t/  _    _\\\n" +
+                        "\t      o"
+        );
+    }
+}
